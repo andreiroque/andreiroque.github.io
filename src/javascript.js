87af4881ca31b7061c490872c361d037df90724e
andreiroque <119117082+andreiroque@users.noreply.github.com>
2022-11-27 15:44:35 +0800

    Add files via upload

--- src/javascript.js
+++ src/javascript.js
@@ -1,5 +1,7 @@
 
+
 var counter = 0;
+
 
 function clicked(){
     counter++;
@@ -7,6 +9,7 @@ function clicked(){
         document.getElementById("container").style.backgroundColor="black";
         document.getElementById("Text").style.color="white";
         document.getElementById("Button").style.border="1px solid white";
+        document.getElementById("Text").innerHTML = "PUTANGINAMO ROBERT ARCELON INTALAN PAKYU";
     }else if(counter == 2){
         document.getElementById("container").style.backgroundColor="white";
         document.getElementById("Text").style.color="black";
