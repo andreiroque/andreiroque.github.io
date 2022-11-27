6dd890239f3b9346938f1247a713903860cb26e3
andreiroque <119117082+andreiroque@users.noreply.github.com>
2022-11-27 20:00:21 +0800

    Add files via upload

--- src/javascript.js
+++ src/javascript.js
@@ -9,10 +9,12 @@ function clicked(){
         document.getElementById("container").style.backgroundColor="black";
         document.getElementById("Text").style.color="white";
         document.getElementById("Button").style.border="1px solid white";
+        document.getElementById("Text").innerHTML = "How exciting :>";
     }else if(counter == 2){
         document.getElementById("container").style.backgroundColor="white";
         document.getElementById("Text").style.color="black";
         document.getElementById("Button").style.border="2px solid black";
+        document.getElementById("Text").innerHTML = "This is my First Website!";
         counter = 0;
     }
 }
