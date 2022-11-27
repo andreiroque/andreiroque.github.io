dd060232c367690f713aaa07044d283590cb301c
andreiroque <119117082+andreiroque@users.noreply.github.com>
2022-11-27 11:29:39 +0800

    Add files via upload

---
+++ src/javascript.js
@@ -0,0 +1,16 @@
+
+var counter = 0;
+
+function clicked(){
+    counter++;
+    if(counter == 1){
+        document.getElementById("container").style.backgroundColor="black";
+        document.getElementById("Text").style.color="white";
+        document.getElementById("Button").style.border="1px solid white";
+    }else if(counter == 2){
+        document.getElementById("container").style.backgroundColor="white";
+        document.getElementById("Text").style.color="black";
+        document.getElementById("Button").style.border="2px solid black";
+        counter = 0;
+    }
+}
